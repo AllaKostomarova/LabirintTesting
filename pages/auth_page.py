561cd5490dd8cd1f7c45fee8580c170e70c3b82f
Alla Kostomarova <86626601+AllaKostomarova@users.noreply.github.com>
2021-12-23 21:04:17 +0300

    Update auth_page.py

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,12 +1,7 @@
-from urllib3.util import url
-
 from config import TestData
 from pages.locators import AuthLocators
 from pages.base_page import BasePage
 
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
